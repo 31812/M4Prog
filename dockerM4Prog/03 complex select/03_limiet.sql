caeda89a3733faf826d84102986c2b7f749eefaf
31812 <31812@ma-web.nl>
2024-05-24 12:56:20 +0200

    week3

---
+++ dockerM4Prog/03 complex select/03_limiet.sql
@@ -0,0 +1,3 @@
+SELECT * FROM `huisdierdata` WHERE 1 LIMIT 2
+
+SELECT * FROM `huisdierdata` WHERE 1 LIMIT 3
---
+++ dockerM4Prog/03 complex select/03_rekenen.sql
@@ -0,0 +1,5 @@
+SELECT SUM(leeftijd) FROM `huisdierdata` WHERE 1
+
+SELECT sum(leeftijd), avg(leeftijd) FROM `huisdierdata` WHERE 1
+
+SELECT avg(leeftijd) as gemiddeldeleeftijd FROM `huisdierdata` WHERE 1;
