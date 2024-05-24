28b7f1eab349ed9ab2780e0774c75fda04b69612
31812 <31812@ma-web.nl>
2024-05-24 10:19:21 +0200

    week2

---
+++ dockerM4Prog/02 data/02_huisdierenselect.sql
@@ -0,0 +1,9 @@
+SELECT dier FROM `huisdierdata` WHERE 1
+
+UPDATE huisdierdata SET dier = 'Geit' WHERE dier = 'Slang';
+
+SELECT dier,ras FROM `huisdierdata` WHERE 1
+
+SELECT gender,leeftijd,geboortedatum FROM `huisdierdata` WHERE 1
+
+SELECT ras,dier FROM `huisdierdata` WHERE 1
---
+++ dockerM4Prog/02 data/huisdierdata.sql
@@ -0,0 +1,7 @@
+INSERT INTO `huisdierdata` (`dier`, `ras`, `gender`, `leeftijd`, `geboortedatum`) 
+VALUES 
+('Slang', 'Ratelslang', 'Vrouw', '6 jaar', '10-05-2003'),
+('Hond', 'Bulldog', 'Man', '3 jaar', '21-07-2011'),
+('Kat', 'Calico', 'Vrouw', '7 jaar', '16-01-2009'),
+('Vis', 'Goudvis', 'Vrouw', '2 jaar', '09-11-2021'),
+('Tijger', 'Sneeuwtijger', 'Man', '12 jaar', '01-03-2004');
